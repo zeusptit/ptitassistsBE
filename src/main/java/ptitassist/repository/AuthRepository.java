fc0374a552f3be0bbcc90bb89b523bd3e6ab41fa
Trần Phú Cường <94471615+zeusptit@users.noreply.github.com>
2023-08-15 20:59:10 +0700

    Update AuthRepository.java

--- src/main/java/ptitassist/repository/AuthRepository.java
+++ src/main/java/ptitassist/repository/AuthRepository.java
@@ -11,9 +11,11 @@ import java.util.concurrent.ExecutionException;
 
 @Repository
 public class AuthRepository {
+    //Phương thức truy xuất User với username
     public User getUser(String name) throws ExecutionException, InterruptedException {
         Firestore dbFireStore = FirestoreClient.getFirestore();
         DocumentReference documentReference = dbFireStore.collection("users").document(name);
+        //Dữ liệu được truy xuất từ collection "users" và document "name"
         ApiFuture<DocumentSnapshot> future = documentReference.get();
         DocumentSnapshot documentSnapshot = future.get();
         User user = null;
@@ -23,6 +25,7 @@ public class AuthRepository {
         return user;
     }
 
+    //Tạo user với và được lưu trữ vào trong firestore 
     public String createUser(User user) throws ExecutionException, InterruptedException {
         Firestore dbFireStore = FirestoreClient.getFirestore();
         CollectionReference usersCollection = dbFireStore.collection("users");
@@ -30,6 +33,7 @@ public class AuthRepository {
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
+    //Xác thực sự tồn tại user cho đăng kí
     public Boolean checkCredentials(Auth auth) throws ExecutionException, InterruptedException {
         User foundUser = getUser(auth.getName());
         if (foundUser != null) {
