2963cd1c8554ee009553bc9e08f28dc65ce12782
Trần Phú Cường <94471615+zeusptit@users.noreply.github.com>
2023-08-15 21:01:23 +0700

    Update UserService.java

--- src/main/java/ptitassist/service/UserService.java
+++ src/main/java/ptitassist/service/UserService.java
@@ -10,7 +10,9 @@ import java.util.concurrent.ExecutionException;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+    private final UserRepository userRepository
+
+    //Phương thức cập nhật thông tin user theo username name
     public User updateUser(String username, User updateUser) throws ExecutionException, InterruptedException {
         return userRepository.updateUser(username, updateUser);
     }
